ab27425f2c5c3f560fa3383bbd69e4ee196c6f47
ewgo <ewelina.gonda@gft.com>
2016-08-24 14:40:43 +0200

    partly working livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
 var gulp = require('gulp');
+var livereload = require('gulp-livereload');
+var runSequence = require('run-sequence');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('js', function() {
+  gulp.src('app/')
+    .pipe(gulp.dest('app'))
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('app/', ['js']);
+});
+
+gulp.task('dev', function(cb) {
+
+  runSequence(['watch'], cb);
+
 });
